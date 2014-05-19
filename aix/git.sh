ad3ad71b34b54e379520c60e9635fe6a7d1a7d97
Mișu Moldovan <dumol@chevah.com>
2014-05-20 01:39:15 +0300

    Initial adaptations for git setup.

--- aix/git.sh
+++ aix/git.sh
@@ -3,30 +3,37 @@
 #
 # Add git path to ~/bash_profile path... if it exists.
 #
-# Requires wget and a compiler
+# Requires wget and sudo
 #
 
-GIT_VERSION=1.7.10
+GIT_VERSION=1.9.0
 
 # Folder where git will be installed.
 INSTALL_FOLDER=~/.local
 
 GIT_FOLDER=git-${GIT_VERSION}
-GIT_TAR=${GIT_FOLDER}.tar
-GIT_TAR_GZ=${GIT_TAR}.gz
+GIT_TAR_GZ=${GIT_FOLDER}.tar.gz
 GIT_REMOTE_ARCHIVE=http://git-core.googlecode.com/files/${GIT_TAR_GZ}
 
 # Absolute path to a ginstall compatible install file.
 # We use a script that wraps ginstall arround aix install.
-INSTALL_SCRIPT=~/chevah/deps/src/chevah-bootstrap/aix/install.aix.sh
+INSTALL_SCRIPT=$INSTALL_FOLDER/install.aix.sh
+wget https://raw.githubusercontent.com/chevah/chevah-os-bootstrap/master/aix/install.aix.sh -O $INSTALL_SCRIPT
+chmod +x $INSTALL_SCRIPT
+
+# Get build deps
+wget ftp://ftp.software.ibm.com/aix/freeSoftware/aixtoolbox/RPMS/ppc/gcc/gcc-4.2.0-3.aix5.3.ppc.rpm
+sudo rpm -i gcc-4.2.0-3.aix5.3.ppc.rpm
+wget ftp://ftp.software.ibm.com/aix/freeSoftware/aixtoolbox/RPMS/ppc/zlib/zlib-1.2.3-4.aix5.2.ppc.rpm
+wget ftp://ftp.software.ibm.com/aix/freeSoftware/aixtoolbox/RPMS/ppc/zlib/zlib-devel-1.2.3-4.aix5.2.ppc.rpm
+sudo rpm -i zlib-*.rpm
 
 # Delete already existent git build folder and archive.
 rm -rf $GIT_FOLDER
-rm -rf $GIT_TAR
+rm -rf $GIT_TAR_GZ
 
 wget ${GIT_REMOTE_ARCHIVE}
-gunzip $GIT_TAR_GZ
-tar -xf $GIT_TAR
+gunzip -c $GIT_TAR_GZ | tar -xf -
 
 cd $GIT_FOLDER
 
