bd8c8bda15dd1f4b93c359cc228e58e2a386c779
Mișu Moldovan <dumol@chevah.com>
2014-05-30 21:42:54 +0300

    Cleaned up the code a bit.

--- aix/git.sh
+++ aix/git.sh
@@ -3,7 +3,7 @@
 # Downloads, compiles and installs GIT.
 # Adds git path to ~/bash_profile path... if it exists.
 # Requires wget and sudo.
-# Installs gcc, zlib, zlib-devel and coreutils RPMs from AIX's toolbox.
+# Installs gcc, zlib, zlib-devel and coreutils.
 #
 
 GIT_VERSION=1.9.0
@@ -15,15 +15,20 @@ GIT_FOLDER=git-${GIT_VERSION}
 GIT_TAR_GZ=${GIT_FOLDER}.tar.gz
 GIT_REMOTE_ARCHIVE=http://git-core.googlecode.com/files/${GIT_TAR_GZ}
 
+# Get build deps from AIX's toolbox.
+IBM_FTP_LINK_BASE="ftp://ftp.software.ibm.com/aix/freeSoftware/aixtoolbox/RPMS/ppc"
+RPM_DEPS="gcc-4.2.0-3.aix5.3.ppc.rpm \
+          coreutils-5.0-2.aix5.1.ppc.rpm \
+          zlib-1.2.3-4.aix5.2.ppc.rpm \
+          zlib-devel-1.2.3-4.aix5.2.ppc.rpm"
+for RPM_FILE in $RPM_DEPS; do
+    RPM_DIR=`echo $RPM_FILE | cut -d\- -f1`
+    wget "$IBM_FTP_LINK_BASE"/"$RPM_DIR"/"$RPM_FILE"
+    sudo rpm -i $RPM_FILE && rm $RPM_FILE
+done
+
 # Absolute path to a ginstall from the coreutils package.
 INSTALL_SCRIPT="/usr/linux/bin/install"
-
-# Get build deps
-wget -q ftp://ftp.software.ibm.com/aix/freeSoftware/aixtoolbox/RPMS/ppc/gcc/gcc-4.2.0-3.aix5.3.ppc.rpm
-wget -q ftp://ftp.software.ibm.com/aix/freeSoftware/aixtoolbox/RPMS/ppc/coreutils/coreutils-5.0-2.aix5.1.ppc.rpm
-wget -q ftp://ftp.software.ibm.com/aix/freeSoftware/aixtoolbox/RPMS/ppc/zlib/zlib-1.2.3-4.aix5.2.ppc.rpm
-wget -q ftp://ftp.software.ibm.com/aix/freeSoftware/aixtoolbox/RPMS/ppc/zlib/zlib-devel-1.2.3-4.aix5.2.ppc.rpm
-sudo rpm -i gcc-4.2.0-3.aix5.3.ppc.rpm coreutils-5.0-2.aix5.1.ppc.rpm zlib-1.2.3-4.aix5.2.ppc.rpm zlib-devel-1.2.3-4.aix5.2.ppc.rpm
 
 # Delete already existent git build folder and archive.
 rm -rf $GIT_FOLDER
